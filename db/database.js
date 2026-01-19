fb902187e2623e9e519abd1b1804d8f489d077f2
Anjarakely <moratianarajaonah@gmail.com>
2026-01-19 14:20:05 +0300

    base infinitifree

--- db/database.js
+++ db/database.js
@@ -1,61 +1,34 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true
-  }
+  port: process.env.DB_PORT || 3306
 });
 
 connection.connect(err => {
-  if (err) {
-    console.error("❌ Erreur MySQL :", err);
-  } else {
-    console.log("✅ Connecté à MySQL (Aiven)");
-  }
+  if (err) console.error('Erreur de connexion MySQL:', err);
+  else console.log('Connecté à MySQL InfinityFree !');
 });
 
 module.exports = connection;
 
 
-// // const mysql = require('mysql2');
 
-// // const connection = mysql.createConnection({
-// //   host: 'localhost',
-// //   user: 'root',
-// //   password: '',  // ton mot de passe
-// //   database: 'caissepro' // nom de la DB
-// // });
-
-// // connection.connect(err => {
-// //   if (err) throw err;
-// //   console.log('Connecté à la base de données MySQL');
-// // });
-
-// // module.exports = connection;
+// // db.js
 // const mysql = require('mysql2');
-
 // const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'caissepro'
 // });
 
 // connection.connect(err => {
-//   if (err) {
-//     console.error('❌ Erreur MySQL :', err);
-//     return;
-//   }
-//   console.log('✅ Connecté à MySQL (Aiven)');
+//   if (err) throw err;
+//   console.log('Connecté à MySQL');
 // });
 
 // module.exports = connection;
